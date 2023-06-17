d446b01d167c61021a2d7d4fd775aab5261287bc
Alex <mboutchouangalex+github@gmail.com>
2023-06-17 13:59:39 +0200

    Join Frontend and Backend

---
+++ frontend/api/messages.ts
@@ -0,0 +1,20 @@
+export const getMessages = async () => {
+  const messages = await (await fetch(`${process.env.NEXT_PUBLIC_API_BASE_ENDPOINT}/messages`)).json();
+  return messages;
+}
+
+export const postMessages = async (category_id, message) => {
+  try{
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_ENDPOINT}/messages`,{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({category_id, text: message})
+    })
+    return resp.json();
+  } catch (e){
+    console.log(e)
+    return null;
+  }
+}
