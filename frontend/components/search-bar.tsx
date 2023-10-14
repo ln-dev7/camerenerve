9e0391f313e44dd7a0ba940029801b48fc4fd40c
ln-dev7 <leonelngoya@gmail.com>
2023-10-14 10:00:01 +0100

    disabled search-bar

--- frontend/components/search-bar.tsx
+++ frontend/components/search-bar.tsx
@@ -3,5 +3,5 @@ import React from "react"
 import { Input } from "@/components/ui/input"
 
 export default function SearchBar() {
-  return <Input className="w-full" type="text" placeholder="Recherche un message" />
+  return <Input disabled className="w-full" type="text" placeholder="Recherche un message" />
 }
